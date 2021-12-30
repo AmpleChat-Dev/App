192edd39c30956cf6909a2e17297b4a65c91ea31
Boris <Borismulder@outlook.com>
2021-12-30 23:12:27 +0100

    Update packet.js

--- src/packet.js
+++ src/packet.js
@@ -7,7 +7,10 @@ export const LOGIN_PACKET = 5
 export const LOGIN_RESPONSE = 6
 export const ACCOUNT_INFO_PACKET = 7
 export const ACCOUNT_INFO_RESPONSE = 8
-
-function parsePacket(rawData) {
-    return JSON.parse(rawData)
-}
+export const CONTACT_REQUEST_PACKET = 9
+export const CONTACTS_PACKET = 10
+export const DISCONNECT_PACKET = 11
+export const SEND_CONTACT_REQUEST_PACKET = 12
+export const SEND_CONTACT_REQUEST_PACKET_RESPONSE = 13
+export const CHAT_MESSAGE_PACKET = 14
+export const CHAT_MESSAGES_PACKET = 15
